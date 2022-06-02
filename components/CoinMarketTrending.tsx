07890b5a03793757f98ba3e7584858fad0ef0726
M-max125 <c.marcu60@yahoo.com>
2022-06-02 14:10:48 +0300

    Initial commit

---
+++ components/CoinMarketTrending.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CoinMarketTrending = () => {
+  return (
+    <div>CoinMarketTrending</div>
+  )
+}
+
+export default CoinMarketTrending
---
+++ components/CoinmarketTable.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CoinmarketTable = () => {
+  return (
+    <div>CoinmarketTable</div>
+  )
+}
+
+export default CoinmarketTable
